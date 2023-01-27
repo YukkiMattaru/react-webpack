a6f7eea5d3847c9a31c92e0ac5c2e3eb9207e44b
YukkiMattaru <gaidukov200078@gmail.com>
2023-01-28 01:15:34 +0300

    feat: Добавлен prettier конфиг, prettify обработка файлов

--- src/components/kit/icons/index.ts
+++ src/components/kit/icons/index.ts
@@ -1,3 +1,3 @@
-export {default as HomeOutline} from './HomeOutline.svg'
-export {default as RedoOutline} from './RedoOutline.svg'
-export {default as UnlockOutline} from './UnlockOutline.svg'
+export { default as HomeOutline } from './HomeOutline.svg';
+export { default as RedoOutline } from './RedoOutline.svg';
+export { default as UnlockOutline } from './UnlockOutline.svg';
