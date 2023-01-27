6ada8e1ecc7078012444cc97a4d0a4c38e4aa9d4
YukkiMattaru <gaidukov200078@gmail.com>
2023-01-27 23:35:20 +0300

    feat: Встроен плагин для хуков реакта для линтера, линтигнор

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,20 @@ module.exports = {
         node: true,
         es2022: true,
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react/jsx-runtime"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended"
+    ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
         jsx: true
     },
-    plugins: ['@typescript-eslint', 'react'],
+    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
     rules: {
         // игнорируем полностью, так как юзаем эти конструкции только в webpack.config.js
         "@typescript-eslint/no-var-requires": 0
