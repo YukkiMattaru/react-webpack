18a35b7582c20fa49bbabd71f5f8ff30d3bd256c
YukkiMattaru <gaidukov200078@gmail.com>
2023-01-28 16:04:15 +0300

    feat: Stylelint configuration

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  plugins: ['stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss', 'stylelint-config-styled-components'],
+  rules: {
+    // @NOTE: This conflicts with styled-components
+    'no-empty-source': null,
+  },
+};
